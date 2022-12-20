987ab5db322872cbeb472e85e4eacc49ed1b1dac
Pedram Agand <pagand@sfu.ca>
2022-12-19 17:04:49 -0800

    Changing the burn-in time

---
+++ experiments/soft-contact/Inv_kinematic.c
@@ -0,0 +1,136 @@
+/*
+ */
+
+
+
+/*
+ * You must specify the S_FUNCTION_NAME as the name of your S-function
+ * (i.e. replace sfuntmpl_basic with the name of your S-function).
+ */
+
+#define S_FUNCTION_NAME  Inv_kinematic
+#define S_FUNCTION_LEVEL 2
+#define param_1 *mxGetPr(ssGetSFcnParam(S,0))
+#define param_2 *mxGetPr(ssGetSFcnParam(S,1))
+
+
+/*
+ * Need to include simstruc.h for the definition of the SimStruct and
+ * its associated macro definitions.
+ */
+#include "simstruc.h"
+#include "math.h"
+   
+
+/*====================*
+ * S-function methods *
+ *====================*/
+
+/* Function: mdlInitializeSizes ===============================================
+ * Abstract:
+ *    The sizes information is used by Simulink to determine the S-function
+ *    block's characteristics (number of inputs, outputs, states, etc.).
+ */
+static void mdlInitializeSizes(SimStruct *S)
+{
+    /* See sfuntmpl_doc.c for more details on the macros below */
+
+    ssSetNumSFcnParams(S, 2);  /* Number of expected parameters */
+    if (ssGetNumSFcnParams(S) != ssGetSFcnParamsCount(S)) {
+        /* Return if number of expected != number of actual parameters */
+        ssSetErrorStatus(S,"Parameter assignment failed! \nplease enter the parameter correctly");
+        return;
+    }
+
+    ssSetNumContStates(S, 0);
+    ssSetNumDiscStates(S, 0);
+
+    if (!ssSetNumInputPorts(S, 2)) return;
+    ssSetInputPortWidth(S, 0, 1);
+    ssSetInputPortWidth(S, 1, 1);
+    ssSetInputPortDirectFeedThrough(S, 0, 1);
+    ssSetInputPortDirectFeedThrough(S, 1, 1);
+
+    if (!ssSetNumOutputPorts(S, 2)) return;
+    ssSetOutputPortWidth(S, 0, 1);
+    ssSetOutputPortWidth(S, 1 , 1);
+
+    ssSetNumSampleTimes(S, 1);
+    ssSetNumRWork(S, 0);
+    ssSetNumIWork(S, 0);
+    ssSetNumPWork(S, 0);
+    ssSetNumModes(S, 0);
+    ssSetNumNonsampledZCs(S, 0);
+
+    /* Specify the sim state compliance to be same as a built-in block */
+    ssSetSimStateCompliance(S, USE_DEFAULT_SIM_STATE);
+
+    ssSetOptions(S, 0);
+
+}
+
+
+
+/* Function: mdlInitializeSampleTimes =========================================
+ * Abstract:
+ *    This function is used to specify the sample time(s) for your
+ *    S-function. You must register the same number of sample times as
+ *    specified in ssSetNumSampleTimes.
+ */
+static void mdlInitializeSampleTimes(SimStruct *S)
+{
+    ssSetSampleTime(S, 0, INHERITED_SAMPLE_TIME);
+    ssSetOffsetTime(S, 0, 0.0);
+
+}
+
+
+/* Function: mdlOutputs =======================================================
+ * Abstract:
+ *    In this function, you compute the outputs of your S-function
+ *    block.
+ */
+static void mdlOutputs(SimStruct *S, int_T tid)
+{
+    real_T alpha,beta,gama,phi;
+    InputRealPtrsType uPtrs1,uPtrs2;
+    real_T *teta1,*teta2;
+    uPtrs1=ssGetInputPortRealSignalPtrs(S,0);
+    uPtrs2=ssGetInputPortRealSignalPtrs(S,1);
+    teta1=ssGetOutputPortRealSignal(S,0);
+    teta2=ssGetOutputPortRealSignal(S,1);
+    gama=*uPtrs1[0];
+    phi=*uPtrs2[0];
+    alpha=param_1;
+    beta=param_2;
+    
+    
+    *teta1 = phi+acos((cos(beta)-cos(gama)*cos(alpha))/(sin(gama)*sin(alpha)));
+    *teta2 = phi-acos((cos(beta)-cos(gama)*cos(alpha))/(sin(gama)*sin(alpha)));
+     
+}
+
+/* Function: mdlTerminate =====================================================
+ * Abstract:
+ *    In this function, you should perform any actions that are necessary
+ *    at the termination of a simulation.  For example, if memory was
+ *    allocated in mdlStart, this is the place to free it.
+ */
+static void mdlTerminate(SimStruct *S)
+{
+}
+
+
+/*======================================================*
+ * See sfuntmpl_doc.c for the optional S-function methods *
+ *======================================================*/
+
+/*=============================*
+ * Required S-function trailer *
+ *=============================*/
+
+#ifdef  MATLAB_MEX_FILE    /* Is this file being compiled as a MEX-file? */
+#include "simulink.c"      /* MEX-file interface mechanism */
+#else
+#include "cg_sfun.h"       /* Code generation registration function */
+#endif
